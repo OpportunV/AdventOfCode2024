9fc5b879448e70d1a8aac5422297a65150fec835
OpportunV <RsTGear@gmail.com>
2024-12-06 21:20:14 +0300

    Day 6 full brute force part 2

---
+++ AdventOfCode2024/Days/Day6.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode2024.Models;
+
+namespace AdventOfCode2024.Days;
+
+public class Day6 : Day
+{
+    private readonly char[][] _grid;
+    private readonly int _cols;
+    private readonly int _rows;
+
+    private static readonly List<GridPos2d> _directionsSide =
+    [
+        new(-1, 0),
+        new(0, 1),
+        new(1, 0),
+        new(0, -1)
+    ];
+
+    public Day6()
+    {
+        _grid = GetInput().Select(line => line.ToCharArray()).ToArray();
+        _rows = _grid.Length;
+        _cols = _grid[0].Length;
+    }
+
+    public override string Part1()
+    {
+        var positions = GetPosiblePositions();
+        return positions.Count.ToString();
+    }
+
+    public override string Part2()
+    {
+        var counter = 0;
+        var seen = new HashSet<(GridPos2d, int)>();
+        for (var row = 0; row < _rows; row++)
+        {
+            for (var col = 0; col < _cols; col++)
+            {
+                if (_grid[row][col] == '#')
+                {
+                    continue;
+                }
+
+                var pos = GetStartPos();
+                if (row == pos.Row && col == pos.Col)
+                {
+                    continue;
+                }
+
+                _grid[row][col] = '#';
+                var dirIndex = 0;
+                seen.Clear();
+                while (true)
+                {
+                    if (!seen.Add((pos, dirIndex)))
+                    {
+                        counter++;
+                        break;
+                    }
+
+                    if (!TrySimulateStep(ref pos, ref dirIndex))
+                    {
+                        break;
+                    }
+                }
+
+                _grid[row][col] = '.';
+            }
+        }
+
+        return counter.ToString();
+    }
+
+    private HashSet<GridPos2d> GetPosiblePositions()
+    {
+        var pos = GetStartPos();
+        var dirIndex = 0;
+        var seen = new HashSet<GridPos2d>();
+
+        while (true)
+        {
+            seen.Add(pos);
+            if (!TrySimulateStep(ref pos, ref dirIndex))
+            {
+                break;
+            }
+        }
+        
+        return seen;
+    }
+
+    private bool TrySimulateStep(ref GridPos2d pos, ref int dirIndex)
+    {
+        var newPos = pos + _directionsSide[dirIndex];
+        if (!IsInside(newPos))
+        {
+            return false;
+        }
+
+        if (_grid[newPos.Row][newPos.Col] == '#')
+        {
+            dirIndex = NextDirIndex(dirIndex);
+        }
+        else
+        {
+            pos = newPos;
+        }
+
+        return true;
+    }
+
+    private int NextDirIndex(int dirIndex)
+    {
+        return (dirIndex + 1) % _directionsSide.Count;
+    }
+
+    private bool IsInside(GridPos2d pos)
+    {
+        return pos.Row >= 0 && pos.Row < _cols && pos.Col >= 0 && pos.Col < _rows;
+    }
+
+    private GridPos2d GetStartPos()
+    {
+        for (var i = 0; i < _rows; i++)
+        {
+            for (var j = 0; j < _cols; j++)
+            {
+                if (_grid[i][j] == '^')
+                {
+                    return new GridPos2d(i, j);
+                }
+            }
+        }
+
+        throw new ArgumentOutOfRangeException(nameof(_grid), "Starting position is not found");
+    }
+}
